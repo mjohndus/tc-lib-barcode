0f62f25306c1c69384ba7c688d8fb99ea94be276
M.John <m.john@fn.de>
2026-06-27 11:38:58 +0200

    Update CodeOneOne.php

--- src/Type/Linear/CodeOneOne.php
+++ src/Type/Linear/CodeOneOne.php
@@ -140,6 +140,7 @@ class CodeOneOne extends \Com\Tecnick\Barcode\Type\Linear
         $this->ncols = 0;
         $this->nrows = 1;
         $this->bars = [];
+        $this->sbars = [];
         $this->formatCode();
         $clen = \strlen($this->extcode);
         for ($chr = 0; $chr < $clen; ++$chr) {
@@ -152,6 +153,9 @@ class CodeOneOne extends \Com\Tecnick\Barcode\Type\Linear
                 $bar_width = $this->getBarWidth($char, $pos);
                 if (($pos % 2) === 0 && $bar_width > 0) {
                     $this->bars[] = [$this->ncols, 0, $bar_width, 1];
+                }
+                if ((($pos % 2) == 1) && ($bar_width > 0)) {
+                    $this->sbars[] = [$this->ncols, 0, $bar_width, 1];
                 }
 
                 $this->ncols += $bar_width;
