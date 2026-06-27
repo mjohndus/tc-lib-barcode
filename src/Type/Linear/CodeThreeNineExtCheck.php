8fd230265febe2eab185813fe1ac9d91b6e393cb
M.John <m.john@fn.de>
2026-06-27 11:50:42 +0200

    Update CodeThreeNineExtCheck.php

--- src/Type/Linear/CodeThreeNineExtCheck.php
+++ src/Type/Linear/CodeThreeNineExtCheck.php
@@ -351,7 +351,7 @@ class CodeThreeNineExtCheck extends \Com\Tecnick\Barcode\Type\Linear
      */
     protected function formatCode(): void
     {
-        $code = $this->getExtendCode(\strtoupper($this->code));
+        $code = $this->getExtendCode($this->code);
         $this->extcode = '*' . $code . $this->getChecksum($code) . '*';
     }
 
