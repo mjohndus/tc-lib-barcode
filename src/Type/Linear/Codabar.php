dd59072cac1f0bccaa98db0f744d38b7ef068964
M.John <m.john@fn.de>
2026-06-27 09:36:41 +0200

    Update Codabar.php

--- src/Type/Linear/Codabar.php
+++ src/Type/Linear/Codabar.php
@@ -94,6 +94,7 @@ class Codabar extends \Com\Tecnick\Barcode\Type\Linear
         $this->ncols = 0;
         $this->nrows = 1;
         $this->bars = [];
+        $this->sbars = [];
         $this->formatCode();
         $clen = \strlen($this->extcode);
         for ($chr = 0; $chr < $clen; ++$chr) {
@@ -106,6 +107,9 @@ class Codabar extends \Com\Tecnick\Barcode\Type\Linear
                 $bar_width = $this->getBarWidth($char, $pos);
                 if (($pos % 2) === 0) {
                     $this->bars[] = [$this->ncols, 0, $bar_width, 1];
+                }
+                if (($pos % 2) == 1) {
+                    $this->sbars[] = [$this->ncols, 0, $bar_width, 1];
                 }
 
                 $this->ncols += $bar_width;
