3eeb31265196754f8ccc99214856e3eddf70534a
M.John <m.john@fn.de>
2026-06-27 11:46:05 +0200

    Update CodeThreeNineExtCheck.php

--- src/Type/Linear/CodeThreeNineExtCheck.php
+++ src/Type/Linear/CodeThreeNineExtCheck.php
@@ -365,6 +365,7 @@ class CodeThreeNineExtCheck extends \Com\Tecnick\Barcode\Type\Linear
         $this->ncols = 0;
         $this->nrows = 1;
         $this->bars = [];
+        $this->sbars = [];
         $this->formatCode();
         $clen = \strlen($this->extcode);
         for ($chr = 0; $chr < $clen; ++$chr) {
@@ -379,10 +380,14 @@ class CodeThreeNineExtCheck extends \Com\Tecnick\Barcode\Type\Linear
                 if (($pos % 2) === 0 && $bar_width > 0) {
                     $this->bars[] = [$this->ncols, 0, $bar_width, 1];
                 }
+                if ((($pos % 2) == 1) && ($bar_width > 0)) {
+                    $this->sbars[] = [$this->ncols, 0, $bar_width, 1];
+                }
 
                 $this->ncols += $bar_width;
             }
 
+            $this->sbars[] = [$this->ncols, 0, 1, 1];
             // intercharacter gap
             ++$this->ncols;
         }
