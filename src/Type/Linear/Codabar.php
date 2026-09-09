fbc01157d8afbc5587cbeeabc21f4e8233416dfb
M.John <m.john@fn.de>
2026-09-09 20:31:08 +0200

    Update Codabar.php

--- src/Type/Linear/Codabar.php
+++ src/Type/Linear/Codabar.php
@@ -122,7 +122,7 @@ class Codabar extends \Com\Tecnick\Barcode\Type\Linear
                 if (($pos % 2) === 0) {
                     $this->bars[] = [$this->ncols, 0, $bar_width, 1];
                 }
-                if (($pos % 2) == 1) {
+                if (($pos % 2) === 1) {
                     $this->sbars[] = [$this->ncols, 0, $bar_width, 1];
                 }
 
