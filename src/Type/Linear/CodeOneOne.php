b4c0a6050ca36c64adb5c06d1043425474d7f0e9
M.John <m.john@fn.de>
2026-09-09 20:30:36 +0200

    Update CodeOneOne.php

--- src/Type/Linear/CodeOneOne.php
+++ src/Type/Linear/CodeOneOne.php
@@ -167,7 +167,7 @@ class CodeOneOne extends \Com\Tecnick\Barcode\Type\Linear
                 if (($pos % 2) === 0 && $bar_width > 0) {
                     $this->bars[] = [$this->ncols, 0, $bar_width, 1];
                 }
-                if ((($pos % 2) == 1) && ($bar_width > 0)) {
+                if ((($pos % 2) === 1) && ($bar_width > 0)) {
                     $this->sbars[] = [$this->ncols, 0, $bar_width, 1];
                 }
 
